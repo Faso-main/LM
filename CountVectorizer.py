deac82c28ad4921c8696b05c1dc63e39c515147c
Faso-main <faso.312@yandex.ru>
2025-01-22 10:54:51 +0400

    фвв мусещкшяук учфьзду / 10Ж54 22_1_24

--- CountVectorizer.py
+++ CountVectorizer.py
@@ -1,16 +1,17 @@
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
-sents = ['coronavirus is a highly infectious disease',
-   'coronavirus affects older people the most',
-   'older people are at high risk due to this disease']
+sents = ['люблю собак',
+   'люди нелюди так сказать',
+   'самое высокое здание в мире было спроектировано с моего пениса']
 
 cv = CountVectorizer()
 cv2_2 = CountVectorizer(ngram_range=(2,2))
-
 
 X = cv.fit_transform(sents)
 X = X.toarray()
 
 print(X)
-
 print(sorted(cv.vocabulary_.keys()))
+
+tfidf = TfidfVectorizer()
+transformed = tfidf.fit_transform(sents)
